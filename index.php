c67152b248affdeb315115f4ba08d5ccde08579f
BinojBI <binojishara21@gmail.com>
2020-02-24 23:59:01 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+include('includes/header.php');
+?>
+    <h5>Hello world</h5>
+    <button type="button" class="btn btn-warning">Warning</button>
+    <?php
+include('includes/footer.php');
+?>
